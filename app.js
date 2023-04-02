9529fc6b6ef0cdef6885f10ab3c65d6ec7c1b509
guanchang98 <guanchangst@163.com>
2023-04-02 01:02:06 -0700

    A8 Section 5 completed

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 import express from 'express';
-import HelloController from "./controllers/hello-controller.js";
-
+import cors from 'cors';
 const app = express();
+app.use(cors());
+app.use(express.json());
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+import TuitController from "./controllers/tuits/tuits-controller.js";
+
+
 HelloController(app);
+UserController(app);
+TuitController(app);
 app.listen(4000);
